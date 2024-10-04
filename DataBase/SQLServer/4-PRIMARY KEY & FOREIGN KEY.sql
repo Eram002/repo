654cf66138a3f328f1ce03fdc6980123f0f2eaa1
Eram002 <67386289+Eram002@users.noreply.github.com>
2024-10-04 20:22:15 +0400

    Update 4-PRIMARY KEY & FOREIGN KEY.sql

--- DataBase/SQLServer/4-PRIMARY KEY & FOREIGN KEY.sql
+++ DataBase/SQLServer/4-PRIMARY KEY & FOREIGN KEY.sql
@@ -9,12 +9,12 @@ CREATE TABLE Person
 );
 
 INSERT INTO Person (ID, Name,Email,GenderID) VALUES
- (1,'Jade','jade123@gmail.com',2),
- (2,'Mary','mary43@gmail.com',3),
- (3,'Martin','mar@m.com',1),
- (4,'Rob','rob32@gmail.com',NULL),
- (5,'Mary','mary123@gmail.com',2),
- (6,'Kristin','kristin@gmail.com',NULL);
+                  (1,'Jade','jade123@gmail.com',2),
+		  (2,'Mary','mary43@gmail.com',3),
+		  (3,'Martin','mar@m.com',1),
+ 		  (4,'Rob','rob32@gmail.com',NULL),
+		  (5,'Mary','mary123@gmail.com',2),
+		  (6,'Kristin','kristin@gmail.com',NULL);
 
  SELECT * FROM Person;
  SELECT * FROM Gender;
@@ -29,9 +29,7 @@ CREATE TABLE Gender
 );
 
 INSERT INTO Gender (ID, Gender ) VALUES 
- (1,'Male'),
- (2,'Female'),
- (3,'Unknown');
+ (1,'Male'), (2,'Female'), (3,'Unknown');
 
 
 --MARK A GenderID AS Foreign key--
@@ -58,10 +56,10 @@ VALUES (1,'John'),(2,'jane'),(3,'BOb');
 
 CREATE TABLE Orders 
 (
-		OrderId INT PRIMARY KEY,
-		Ordernumber INT NOT NULL,
-		ID INT NOT NULL,
-		FOREIGN KEY (ID) References Customer(ID)
+	OrderId INT PRIMARY KEY,
+	Ordernumber INT NOT NULL,
+	ID INT NOT NULL,
+	FOREIGN KEY (ID) References Customer(ID)
 
 );
 
@@ -90,14 +88,14 @@ CREATE TABLE Persons
 	ID INT NOT NULL,
 	FirstName NVARCHAR(50),
 	LastName  NVARCHAR(50),
-	Age		  INT,
+	Age	  INT,
 	CONSTRAINT  UC_Person UNIQUE (ID,LastName)
 );
 
  INSERT INTO Persons (ID,FirstName,LastName,Age) 
  VALUES (201,'Sam','Smith',29),
-	   (202,'Alan','Will',30),
-	   (203,'Ronaldo','Sam',28);
+        (202,'Alan','Will',30),
+        (203,'Ronaldo','Sam',28);
 
 SELECT * FROM Persons;
 
@@ -125,10 +123,10 @@ CREATE TABLE Staff
 
 INSERT INTO Staff (ID,Name,Department,City,Age,Salary)
 VALUES   (1001,'Emma','IT','Delhi',29,25000),
-		 (1002,'George','IT','Delhi',27,25000),
-		 (1003,'Cristine','HR','Mumbai',31,10000),
-		 (1004,'Seby','TechnicalSupport','Hyderabad',32,12000),
-		 (1005,'Khalid','Sales','Delhi',25,17000);
+	 (1002,'George','IT','Delhi',27,25000),
+	 (1003,'Cristine','HR','Mumbai',31,10000),
+	 (1004,'Seby','TechnicalSupport','Hyderabad',32,12000),
+	 (1005,'Khalid','Sales','Delhi',25,17000);
 
 SELECT * FROM Staff;
 
@@ -152,15 +150,13 @@ CREATE TABLE Emp2
 (
 	ID       INT NOT NULL,
 	Name     NVARCHAR(50),
-	Age	     INT,
+	Age	 INT,
 	Location NVARCHAR(50) DEFAULT 'Delhi'
 );
 
 INSERT INTO Emp2 (ID,Name,Age,Location) VALUES
-				 (1,'Mona',27,DEFAULT),
-				 (2,'Sonam',23,DEFAULT),
-				 (3,'Sam',29,DEFAULT),
-				 (4,'John',21,DEFAULT);
+(1,'Mona',27,DEFAULT), (2,'Sonam',23,DEFAULT),
+(3,'Sam',29,DEFAULT), (4,'John',21,DEFAULT);
 
 SELECT * FROM Emp2;
 
